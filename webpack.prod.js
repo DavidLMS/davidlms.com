df7797e98ff3acaebf090ca0541fa83dcae14eeb
David <17435126+DavidLMS@users.noreply.github.com>
2025-09-23 15:53:50 +0200

    Replace UglifyJs with Terser for JS minification
    Switched from uglifyjs-webpack-plugin to terser-webpack-plugin in package.json and webpack.prod.js for better ES6+ support and improved minification. Updated related configuration and plugin usage accordingly.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const merge = require("webpack-merge");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -15,18 +15,17 @@ module.exports = merge(common, {
 
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
+      new TerserPlugin({
+        parallel: true
       }),
-
-      new MiniCssExtractPlugin({
-        filename: "[name].[hash:5].css",
-        chunkFilename: "[id].[hash:5].css"
-      }),
-
       new OptimizeCSSAssetsPlugin({})
     ]
-  }
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash:5].css",
+      chunkFilename: "[id].[hash:5].css"
+    })
+  ]
 });
